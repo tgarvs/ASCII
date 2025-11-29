99de386bee6f848a3561fd93a5b665150b571e2a
tgarvs <tgarvey12@gmail.com>
2025-11-29 17:32:25 +0000

    adding guy and making him move

--- PYGUY /pyguy.py
+++ PYGUY /pyguy.py
@@ -1,25 +1,27 @@
 
 
-
 # Example file showing a circle moving on screen
 import pygame
 import random
+import copy
 
 
 class Drop :
     def __init__(self) :
 
         self.pos = pygame.Vector2(random.randrange(0, width, 1), random.randrange(0, height, 1))
+        self.prev = copy.deepcopy(self.pos)
         self.y_speed = random.randrange(6, 9, 1)
         self.x_speed = 2
+        self.size = 1
+
+    def display(self) : 
+        pygame.draw.circle(screen, "green", self.pos, self.size)
+        pygame.draw.circle(screen, "green", self.prev, self.size)
+
 
 
     def move(self) :
-        prev = pygame.Vector2(self.pos.x - self.x_speed, self.pos.y - self.y_speed)
- 
-        pygame.draw.circle(screen, "green", self.pos, 1)
-        pygame.draw.circle(screen, "green", prev, 1)
-
         if(self.pos.y < height - self.y_speed) :
             self.pos.y = self.pos.y + self.y_speed
         else :
@@ -30,35 +32,57 @@ class Drop :
         else :
             self.pos.x = 0
 
+        self.prev = pygame.Vector2(self.pos.x - self.x_speed, self.pos.y - self.y_speed)
+
+
 
 
 class Guy :
     def __init__(self) :
-        self.x = screen.x//2
-        self.y = screen.y-1
-        self.velX = 0
-        self.velY = 0
-        self.accX = 0
-        self.accY = 0
+
         self.moveAmt = 1
         self.maxSpeed = 3
+        self.pos = pygame.Vector2(width//2, height-15)
+        self.vel = pygame.Vector2(0, 0)
+        self.acc = pygame.Vector2(0, 0)
+
+        self.text_surface = my_font.render('&', True, "green")
     
-    def display(self, buff) :
-        self.velX += self.accX
-        
-        if self.velX >= self.maxSpeed :
-            self.velX = self.maxSpeed
-        elif self.velX <= -self.maxSpeed :
-            self.velX = -self.maxSpeed
-        
-        self.x += self.velX
 
-        if self.x > len(buff[0]) - abs(self.velX) :
-            self.x = 1 + abs(self.velX)
-        elif self.x < abs(self.velX) :
-            self.x = len(buff[0]) - abs(self.velX)
+    def display(self) :        
+        self.vel.x += self.acc.x
+        
+        if self.vel.x >= self.maxSpeed :
+            self.vel.x = self.maxSpeed
+        elif self.vel.x <= -self.maxSpeed :
+            self.vel.x = -self.maxSpeed
+        
+        self.pos.x += self.vel.x
 
-        buff[self.y][self.x] = "&"
+        if self.pos.x > width - abs(self.vel.x) :
+            self.pos.x = 1 + abs(self.vel.x)
+        elif self.pos.x < abs(self.vel.x) :
+            self.pos.x = width - abs(self.vel.x)
+
+        text_rect = self.text_surface.get_rect()
+        text_rect.center = self.pos
+        screen.blit(self.text_surface, text_rect)
+
+
+    def move(self, keys) :
+        if keys[pygame.K_w]:
+            pass
+        if keys[pygame.K_s]:
+            pass
+
+        if keys[pygame.K_a]:
+            self.acc.x -= 1
+        if keys[pygame.K_d]:
+            self.acc.x += 1
+
+ 
+
+
 
 
 
@@ -70,14 +94,19 @@ class Guy :
 if __name__=="__main__" :
     # pygame setup
     pygame.init()
+    pygame.font.init()
+
     screen = pygame.display.set_mode((1280, 720))
     width, height = screen.get_size()
     clock = pygame.time.Clock()
     running = True
     dt = 0
 
-    player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+    my_font = pygame.font.SysFont('Arial', 30)
+
+    
     rain = [Drop() for _ in range(80)]
+    guy = Guy()
 
 
     while running:
@@ -89,19 +118,16 @@ if __name__=="__main__" :
 
         # fill the screen with a color to wipe away anything from last frame
         screen.fill("black")
-        [drop.move() for drop in rain]
-
-        pygame.draw.circle(screen, "red", player_pos, 40)
+        for drop in rain :
+            drop.display()
+            drop.move()
+        
+        guy.display()
+        print(guy.pos.x)
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
-            player_pos.y -= 300 * dt
-        if keys[pygame.K_s]:
-            player_pos.y += 300 * dt
-        if keys[pygame.K_a]:
-            player_pos.x -= 300 * dt
-        if keys[pygame.K_d]:
-            player_pos.x += 300 * dt
+        guy.move(keys)
+
 
         # flip() the display to put your work on screen
         pygame.display.flip()
